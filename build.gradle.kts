c1a136f52d8a05e7e265605a2896e9dff3f9c839
IOU4 <ouchaibimad000@gmail.com>
2022-12-02 10:08:43 +0100

    generic jakarta api

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,17 +22,12 @@ tasks.register<Exec>("redeploy") {
 tasks.war {
     archiveFileName.set("ROOT.war")
     // exute redeploy task after war task
-    // finalizedBy("redeploy")
+    finalizedBy("redeploy")
 }
 
 dependencies {
     // jakartaee apis
-    compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
-    compileOnly("jakarta.faces:jakarta.faces-api:4.0.1")
-    compileOnly("jakarta.inject:jakarta.inject-api:2.0.1")
-    compileOnly("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
-    compileOnly("jakarta.persistence:jakarta.persistence-api:3.0.0")
-    runtimeOnly("org.hibernate:hibernate-orm-jbossmodules:5.4.33")
+    compileOnly("jakarta.platform:jakarta.jakartaee-api:10.0.0")
 
     // mariadb jdbc driver
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.1.0")
